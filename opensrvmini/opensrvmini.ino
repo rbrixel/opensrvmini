7ab13570cfe7b28e224acae16c1aa625d2bf74f1
René Brixel <rbrixel@gmail.com>
2024-07-24 00:39:51 +0200

    Create opensrvmini.ino

---
+++ opensrvmini/opensrvmini.ino
@@ -0,0 +1,18 @@
+/*
+ * OpenSRVmini
+ * Test MPU6050
+ *
+ * René Brixel/CampingTech
+ * 2024-04-24
+ *
+ * Board: esp32 -> ESP32 Dev Module
+ */
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("Hello World!");
+  delay(2000);
+}
