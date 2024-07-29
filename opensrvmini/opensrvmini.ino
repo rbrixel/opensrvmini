7fe244b4dfad5ed00939cd4c11d05f397b6c605e
René Brixel <rbrixel@gmail.com>
2024-07-29 15:43:28 +0200

    Update opensrvmini.ino

--- opensrvmini/opensrvmini.ino
+++ opensrvmini/opensrvmini.ino
@@ -1,18 +1,50 @@
 /*
  * OpenSRVmini
- * Test MPU6050
+ * Test TFT
  *
  * René Brixel/CampingTech
  * 2024-04-24
  *
- * Board: esp32 -> ESP32 Dev Module
+ * Board: Arduino Nano
  */
 
-void setup() {
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
   Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G); // MPU6050_RANGE_2_G, MPU6050_RANGE_4_G, MPU6050_RANGE_8_G, MPU6050_RANGE_16_G
+  mpu.setGyroRange(MPU6050_RANGE_2000_DEG); // MPU6050_RANGE_250_DEG, MPU6050_RANGE_500_DEG, MPU6050_RANGE_1000_DEG, MPU6050_RANGE_2000_DEG
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ); // MPU6050_BAND_260_HZ, MPU6050_BAND_184_HZ, MPU6050_BAND_94_HZ, MPU6050_BAND_44_HZ, MPU6050_BAND_21_HZ, MPU6050_BAND_10_HZ, MPU6050_BAND_5_HZ
+  
+  Serial.println("");
+  delay(100);
 }
 
 void loop() {
-  Serial.println("Hello World!");
-  delay(2000);
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+
+  Serial.println("");
+  delay(500);
 }
